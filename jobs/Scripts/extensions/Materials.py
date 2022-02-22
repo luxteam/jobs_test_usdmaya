d04ac3c42c0a922834691e73b4e5f555bbc6e370
Piromancer <Den2000_09@mail.ru>
2022-02-22 15:47:15 +0300

    Extensions fixes

--- jobs/Scripts/extensions/Materials.py
+++
@@ -1,7 +0,0 @@
-def init_viewport(case):
-    mel.eval('modelEditor -e -rom \"FireRenderOverride\" modelPanel4')
-    rpr_render(case)
-
-def save_viewport(case):
-    for i in range(0, 100):
-        cmds.refresh(currentView=True, fe='png', fn=WORK_DIR+'/Color/'+case['case']+'.jpg')
