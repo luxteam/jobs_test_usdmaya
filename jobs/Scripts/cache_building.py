c6e6a16fd7bb3d0596642c9a729d42247ded528b
Wieceslaw <slava.astra@yahoo.com>
2022-02-24 10:45:04 +0300

    Add cache building

--- jobs/Scripts/cache_building.py
+++ jobs/Scripts/cache_building.py
@@ -5,29 +5,45 @@ import maya.mel as mel
 
 def main():
     try:
-        if not cmds.pluginInfo("RadeonProRender", query=True, loaded=True):
-            print("[INFO] RadeonProRender plugin is not loaded. Loading RPR plugin ...")
-            cmds.loadPlugin("RadeonProRender")
+        if not cmds.pluginInfo("mayaUsdPlugin", query=True, loaded=True):
+            print("[INFO] mayaUsdPlugin plugin is not loaded. Loading plugin ...")
+            cmds.loadPlugin('mayaUsdPlugin')
     except Exception as err:
-        print("[ERROR] Failed to load RadeonProRender plugin. Error message: {}".format(str(err)))
+        print("[ERROR] Failed to load mayaUsdPlugin plugin. Error message: {}".format(str(err)))
+        cmds.quit(abort=True)
+        
+    try:
+        if not cmds.pluginInfo("mtoh", query=True, loaded=True):
+            print("[INFO] mtoh plugin is not loaded. Loading mtoh plugin ...")
+            cmds.loadPlugin('mtoh')
+    except Exception as err:
+        print("[ERROR] Failed to load mtoh plugin. Error message: {}".format(str(err)))
         cmds.quit(abort=True)
 
-    print("[INFO] RadeonProRender plugin has been successfully loaded.")
+    print("[INFO] mayaUsdPlugin and mtoh plugins has been successfully loaded.")
 
     try:
         print("[INFO] Preparing configurations for rendering...")
-        cmds.setAttr('defaultRenderGlobals.currentRenderer', 'FireRender', type='string')
-        cmds.athenaEnable(ae=False)
-        engine = os.getenv('ENGINE', 'Tahoe')
-        if engine == "Tahoe":
-            cmds.setAttr('RadeonProRenderGlobals.tahoeVersion', 1)
-        elif engine == "Northstar":
-            cmds.setAttr('RadeonProRenderGlobals.tahoeVersion', 2)
+        cmds.setAttr('defaultRenderGlobals.currentRenderer', type='string' 'rprUsdRender')
+        
+        engine = os.getenv('ENGINE', 'Northstar')
+        if engine == 'Tahoe':
+            cmds.setAttr('defaultRenderGlobals.HdRprPlugin_Prod___rpr_mtohns_core_mtohns_renderQuality', 4)
+        elif engine == 'Northstar':
+            cmds.setAttr('defaultRenderGlobals.HdRprPlugin_Prod___rpr_mtohns_core_mtohns_renderQuality', 5)
+        elif engine == 'Hybrid_Low':
+            cmds.setAttr("defaultRenderGlobals.HdRprPlugin_Prod___rpr_mtohns_core_mtohns_renderQuality", 0)
+        elif engine == 'Hybrid_Medium':
+            cmds.setAttr("defaultRenderGlobals.HdRprPlugin_Prod___rpr_mtohns_core_mtohns_renderQuality", 1)
+        elif engine == 'Hybrid_High':
+            cmds.setAttr("defaultRenderGlobals.HdRprPlugin_Prod___rpr_mtohns_core_mtohns_renderQuality", 2)
+        elif engine == 'Hybrid_Pro':
+            cmds.setAttr("defaultRenderGlobals.HdRprPlugin_Prod___rpr_mtohns_core_mtohns_renderQuality", 3)
+            
         print("[INFO] Current render engine: {}.".format(engine))
-        cmds.setAttr("RadeonProRenderGlobals.completionCriteriaSeconds", 0)
         cmds.setAttr('defaultRenderGlobals.imageFormat', 8)
         
-        mel.eval('fireRender -waitForItTwo')
+        mel.eval('rprUsdRender -cam persp -h 800 -w 600 -wft')
         mel.eval('renderIntoNewWindow render')
         
         results_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'Work', 'Results', 'Maya')
